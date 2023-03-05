e8172bd44a9ffeae2f9d085f15b319fd9ff3c4d9
uamadasun <uchennaamadasun@gmail.com>
2023-03-05 08:38:45 -0800

    fixing deployment error

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,27 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [`gatsby-plugin-anchor-links`, `gatsby-plugin-smoothscroll`, `gatsby-transformer-remark`,`gatsby-plugin-image`,
-  `gatsby-plugin-sharp`,
-  `gatsby-transformer-sharp`, // Needed for dynamic images
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      // The unique name for each instance
-      name: `notes`,
-      // Path to the directory
-      path: `${__dirname}/src/notes/`,
-    },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      // The unique name for each instance
-      name: `images`,
-      // Path to the directory
-      path: `${__dirname}/src/images/`,
-    },
-  },
+  plugins: [`gatsby-plugin-anchor-links`, `gatsby-plugin-smoothscroll`,
 
 ],
 
