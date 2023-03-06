2ce6958fc0f8a230bce5bf917d4727e145563b08
uamadasun <uchennaamadasun@gmail.com>
2023-03-06 11:32:18 -0800

    need to test sub form again

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,7 +4,7 @@ export const ContactForm = () => {
   return (
     <div>
         <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success/" >
-            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="form-name" value="bot-field" />
             <div className="relative z-0 w-1/2 mb-6 group">
                 <input type="text" name="name" id="floating_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="floating_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Name</label>
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,13 +1,21 @@
-import React from "react";
-import Layout from "../components/Layout";
+import * as React from "react"
+import { Layout } from "../components/Layout"
 
-export default function Home(){
 
-  return
-    <div>
-      
-        <p>Thank you for your submission!</p>
-      
-    </div>
+export default function Success() {
+
   
+  return <div>
+    <Layout>
+
+      <p>Sucess! I will get back to you within 24 hours.</p>
+    </Layout>
+    <footer>
+      <p className="copy">
+        Built with &#x2661; by Uchenna
+      </p>
+</footer>
+
+    
+    </div>
 }
