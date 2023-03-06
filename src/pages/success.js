23d6f164c570a71db76ef6c1ab437b2e76c78199
uamadasun <uchennaamadasun@gmail.com>
2023-03-06 11:47:24 -0800

    need to test sub form again

--- src/pages/success.js
+++ src/pages/success.js
@@ -7,8 +7,16 @@ const Success = () => {
   
   return <div>
     <Layout>
-
-      <p>Success! I will get back to you within 24 hours.</p>
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>Success! I will get back to you within 24 hours.</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
     </Layout>
     <footer>
       <p className="copy">
