6faf7b2b092eda8258d9f842c853546def8c4583
uamadasun <uchennaamadasun@gmail.com>
2023-03-06 11:40:19 -0800

    need to test sub form again

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ContactForm = () => {
   return (
     <div>
-        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success/" >
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/pages/success/" >
             <input type="hidden" name="form-name" value="bot-field" />
             <div className="relative z-0 w-1/2 mb-6 group">
                 <input type="text" name="name" id="floating_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
