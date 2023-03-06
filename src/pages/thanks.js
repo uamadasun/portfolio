2b322e7b55cd9a7a081a16a568dc75294651a70a
uamadasun <uchennaamadasun@gmail.com>
2023-03-06 11:05:51 -0800

    need to test sub form again

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-const ThankYouPage = () => {
-  <Layout>
-    <h1>Contact</h1>
-    <p>Thank you for your submission!</p>
-  </Layout>
-};
+export default function ThankYouPage(){
 
-export default ThankYouPage;
+  return
+    <div>
+      <Layout>
+        <h1>Contact</h1>
+        <p>Thank you for your submission!</p>
+      </Layout>
+    </div>
+  
+}
