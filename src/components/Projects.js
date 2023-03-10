050bd50905c5b9b9beec067b5064a65012ae86e0
uamadasun <uchennaamadasun@gmail.com>
2023-03-10 10:29:15 -0800

    add links to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,25 +12,26 @@ export const Projects = () => {
     <div>
         <div className="carousel w-1/2 projectMenu">
             <div id="lazyArtis" className="carousel-item w-full">
-                <img src={LazyArtis}
+                <a href="https://github.com/uamadasun/LazyArtis"><img src={LazyArtis}
                     // height={300}
                     alt="lazyartis" 
                     className="w-full" 
-                />
+                /></a>
             </div> 
             <div id="picturest" className="carousel-item w-full">
-                <img src={Picturest}
+                <a href="https://github.com/uamadasun/picturest"><img src={Picturest}
                     // height={300}
                     alt="picturest"
                     className="w-full" 
-                />
+                /></a>
             </div> 
             <div id="strikify" className="carousel-item w-full">
-                <img src={Strikify}
+                <a href="https://github.com/uamadasun/strikify"><img src={Strikify}
                     // height={300}
                     alt="strikify" 
                     className="w-full" 
-                />
+                /></a>
+                
             </div> 
         </div> 
             <div className="flex justify-center w-full py-2 gap-2">
