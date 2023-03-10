bd092ed1d44025e64ed86166a4b55e7555601b03
uamadasun <uchennaamadasun@gmail.com>
2023-03-10 04:26:31 -0800

    fix image sizing issue

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -10,91 +10,109 @@ export const Technologies = () => {
     <div className='technologies'>
         <StaticImage
             src="../images/javascript.png"
+            height={"75px"}
             alt="javascript"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/python.png"
+            height={"75px"}
             alt="python"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/java.png"
+            height={"75px"}
             alt="java"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/html.png"
+            height={"75px"}
             alt="html"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/css.png"
+            height={"75px"}
             alt="css"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/bootstrap.png"
+            height={"75px"}
             alt="bootstrap"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/react.png"
+            height={"75px"}
             alt="react"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/gatsby.png"
+            height={"75px"}
             alt="gatsby"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/express.png"
+            height={"75px"}
             alt="express"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/flask.png"
+            height={"75px"}
             alt="flask"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/nginx.png"
+            height={"75px"}
             alt="nginx"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/spring.png"
+            height={"75px"}
             alt="spring"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/mysql.png"
+            height={"75px"}
             alt="mysql"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/mongodb.png"
+            height={"75px"}
             alt="mongodb"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/graphql.png"
+            height={"75px"}
             alt="graphql"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/aws.png"
+            height={"75px"}
             alt="aws"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/vscode.png"
+            height={"75px"}
             alt="vscode"
             placeholder="tracedSVG"
         />
         <StaticImage
             src="../images/postman.png"
+            height={"75px"}
             alt="postman"
             // placeholder="tracedSVG"
         />
